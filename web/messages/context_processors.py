9238a9999ae89ecc920bff0105894c14de8316a5
Rafael Bankosegger <rafael@bankosegger.at>
2017-06-01 12:15:22 +0200

    Fix login bug with messaging context processors

--- web/messages/context_processors.py
+++ web/messages/context_processors.py
@@ -1,7 +1,10 @@
 from .models import *
 
 def conversationsOfUser(request):
-    unsorted = Conversation.objects.conversationsOf(request.user).all()
+    unsorted = []
+
+    if request.user.is_authenticated():
+        unsorted = Conversation.objects.conversationsOf(request.user).all()
 
     return {
         'conversations': unsorted #sorted(unsorted, key=lambda conv: conv.latest_message().sent_at, reverse=True)[:5]
