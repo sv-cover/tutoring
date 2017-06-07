40e6b0bc3911acb3a50e605536634199b403abac
Rafael Bankosegger <rafael@bankosegger.at>
2017-06-07 10:28:36 +0200

    Add unread_message_count to messages context processor.

--- web/messages/context_processors.py
+++ web/messages/context_processors.py
@@ -1,11 +1,27 @@
 from .models import *
 
 def conversationsOfUser(request):
-    unsorted = []
+    unsorted_conversations = []
+    unread_message_count = 0
 
     if request.user.is_authenticated():
-        unsorted = Conversation.objects.conversationsOf(request.user).all()
+        unsorted_conversations = Conversation.objects.conversationsOf(request.user).all()
+
+        for conv in list(unsorted_conversations):
+            # unread_message_count += 1
+
+            if conv.latest_message().sender == request.user:
+                # unread_message_count += -1
+                continue
+
+            if conv.latest_message().read_by.count() > 0:
+                msg_read_by = list(conv.latest_message().read_by.all())
+                if not request.user in msg_read_by:
+                    unread_message_count += 1
+            else:
+                unread_message_count += 1
 
     return {
-        'conversations': unsorted #sorted(unsorted, key=lambda conv: conv.latest_message().sent_at, reverse=True)[:5]
+        'conversations': unsorted_conversations, #sorted(unsorted, key=lambda conv: conv.latest_message().sent_at, reverse=True)[:5]
+        'unread_message_count': unread_message_count
     }
