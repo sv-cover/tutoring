d010a13b279f4d0e0c556bbcbd03e4f73a04b770
Rafael Bankosegger <rafael@bankosegger.at>
2017-06-07 14:22:00 +0200

    - Bugfix: Messages can be marked 'read now' - Made message list more beautiful - ...

--- web/messages/context_processors.py
+++ web/messages/context_processors.py
@@ -30,9 +30,10 @@ def conversationsOfUser(request):
     conversations_without_messages = filter(lambda conv: conv.latest_message() is None, unsorted_conversations)
     conversations_with_messages = filter(lambda conv: not conv.latest_message() is None, unsorted_conversations)
 
-    sorted_conversations = sorted(conversations_with_messages, key=lambda conv: conv.latest_message().sent_at, reverse=True)[:5]
+    sorted_conversations = sorted(conversations_with_messages, key=lambda conv: conv.latest_message().sent_at, reverse=True)
 
     return {
-        'conversations': list(chain(conversations_without_messages, sorted_conversations)),
+        'conversations': list(chain(conversations_without_messages, sorted_conversations))[:5],
         'unread_message_count': unread_message_count,
+        'archived_conversations_count' : unsorted_conversations.count() - 5,
     }
