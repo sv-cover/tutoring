46e385d0473a42a55f28c1d7e48775b4698b4f4d
Rafael Bankosegger <rafael@bankosegger.at>
2017-11-12 12:32:23 +0100

    Change ALLOWED_HOSTS to only tutoring.svcover.nl

--- web/tutoring/settings.py
+++ web/tutoring/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '<secret>'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tutoring.svcover.nl']
 
 
 # Application definition
