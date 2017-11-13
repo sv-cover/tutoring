f7311988ee7fe0914b277f990ab7891c70cecd8b
Rafael Bankosegger <rafael@bankosegger.at>
2017-11-13 22:27:51 +0100

    Remove 'special' beta key field from authentication form.

--- web/CoverAccounts/forms.py
+++ web/CoverAccounts/forms.py
@@ -7,12 +7,11 @@ class AuthenticationForm(forms.Form):
     """
     email = forms.EmailField(widget=forms.widgets.TextInput)
     password = forms.CharField(widget=forms.widgets.PasswordInput)
-    special = forms.CharField(widget=forms.widgets.PasswordInput)
     user_read_disclaimer = forms.BooleanField(required=True, \
         label='I have read and agree to the <a href="/terms_conditions">terms and conditions</a> for this website.')
 
     class Meta:
-        fields = ['email', 'password', 'special', 'user_read_disclaimer']
+        fields = ['email', 'password', 'user_read_disclaimer']
 
 class SettingsForm(forms.ModelForm):
     class Meta:
