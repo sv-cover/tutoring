cc49df57380b82fd0a8998d1536a6efbacf8f2dc
Rafael Bankosegger <rafael@bankosegger.at>
2018-01-28 14:00:17 +0100

    Update telegram bot uid and make bot messages go to secret url

--- web/telegram_bot/urls.py
+++ web/telegram_bot/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.conf import settings
 
 from . import views
 
+
 app_name = 'telegram_bot'
 urlpatterns = [
-    url(r'^secreturl$', views.webhook, name='webhook'),
+    url(r'^bot%s' % settings.TELEGRAM_BOT_API_TOKEN, views.webhook, name='webhook'),
 ]
