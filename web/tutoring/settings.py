79c1d97e5f4ba48ba5d8a76ce76a8c2126a060cf
Rafael Bankosegger <rafael@bankosegger.at>
2018-01-28 14:00:37 +0100

    Fix dev settings

--- web/tutoring/settings.py
+++ web/tutoring/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '<secret>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['tutoring.svcover.nl', 'api.telegram.org']
+ALLOWED_HOSTS = ['localhost', 'tutoring.svcover.nl', 'api.telegram.org']
 
 
 # Application definition
