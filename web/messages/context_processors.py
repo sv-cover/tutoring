c6a680747c57c02768ec71eebfa3bd3c9cc067b0
rmellema <rene.mellema@gmail.com>
2018-06-11 12:06:06 +0200

    changed a variable name to make more sense

--- web/messages/context_processors.py
+++ web/messages/context_processors.py
@@ -1,32 +1,28 @@
-from itertools import chain
-
-from .models import *
+from .models import Conversation
 
 def conversationsOfUser(request):
     if not request.user.is_authenticated():
         return {}
-    else:
-        unread_message_count = 0
-        conversations = list(Conversation.objects.conversations_of(request.user))
+    unread_conversation_count = 0
+    conversations = list(Conversation.objects.conversations_of(request.user))
 
-        for conv in conversations:
+    for conv in conversations:
 
-            if conv.latest_message() is None:
-                continue
+        if conv.latest_message() is None:
+            continue
 
-            if conv.latest_message().sender == request.user:
-                continue
+        if conv.latest_message().sender == request.user:
+            continue
 
-            if conv.latest_message().read_by.count() > 0:
-                msg_read_by = list(conv.latest_message().read_by.all())
-                if not request.user in msg_read_by:
-                    unread_message_count += 1
-            else:
-                unread_message_count += 1
+        if conv.latest_message().read_by.count() > 0:
+            msg_read_by = list(conv.latest_message().read_by.all())
+            if not request.user in msg_read_by:
+                unread_conversation_count += 1
+        else:
+            unread_conversation_count += 1
 
-
-        return {
-            'recent_conversations': conversations[:5],
-            'unread_message_count': unread_message_count,
-            'conversation_count': len(conversations),
-        }
+    return {
+        'recent_conversations': conversations[:5],
+        'unread_conversation_count': unread_conversation_count,
+        'conversation_count': len(conversations),
+    }
